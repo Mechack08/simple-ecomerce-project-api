1271e3c2a4c140f8f65644d230c013cdf3c842be
8pro <mechackkobius@gmail.com>
2021-09-19 12:26:06 +0300

    adding cors to give access to the backend

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const userRoute = require("./routes/user");
 const userAuth = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -16,12 +18,14 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", userAuth);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5500, () => {
   console.log("Listening on port " + process.env.PORT);
